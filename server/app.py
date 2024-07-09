7644a443f1182e281a991d60312b1409b13db9c0
Abdulhakimkullow <abdulhakim.kullow@student.moringaschool.com>
2024-07-09 10:12:36 +0300

    complete

--- server/app.py
+++ server/app.py
@@ -87,12 +87,24 @@ class CheckSession(Resource):
 class MemberOnlyIndex(Resource):
     
     def get(self):
-        pass
-
+        user_id = session.get('user_id')
+        if not user_id:
+            return {'message': 'Unauthorized access'}, 401
+        articles = Article.query.filter_by(is_member_only=True).all()
+        article_list = [article.to_dict() for article in articles]
+        return jsonify(article_list)
+        
 class MemberOnlyArticle(Resource):
     
     def get(self, id):
-        pass
+        user_id = session.get('user_id')
+        if not user_id:
+            return {'message': 'Unauthorized access'}, 401
+
+        article = Article.query.filter_by(id=id, is_member_only=True).first()
+        if article:
+            return jsonify(article.to_dict()), 200
+        return {'message': 'Article not found or not accessible'}, 200
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(IndexArticle, '/articles', endpoint='article_list')
